755628272aa6ef01c6909880c2979a88a3d01190
TAMARA JERINIC <tamara.jerinic@gmail.com>
2022-10-29 22:55:05 +0200

    Povezani fragmenti login i register. Omogućen njihov prikaz nakon pokretanja aplikacije.

--- Client/BrzoDoLokacije/app/src/main/java/com/example/brzodolokacije/Activities/ActivityLoginRegister.kt
+++ Client/BrzoDoLokacije/app/src/main/java/com/example/brzodolokacije/Activities/ActivityLoginRegister.kt
@@ -2,11 +2,49 @@ package com.example.brzodolokacije.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.example.brzodolokacije.Fragments.FragmentLogin
+import com.example.brzodolokacije.Fragments.FragmentRegister
 import com.example.brzodolokacije.R
+import com.google.android.material.internal.ContextUtils.getActivity
 
 class ActivityLoginRegister : AppCompatActivity() {
+
+    private lateinit var login: Button
+    private lateinit var register: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_register)
+        Log.d("main","123456")
+        login=findViewById<View>(R.id.btnFragmentActivityLRLogin) as Button
+        register=findViewById<View>(R.id.btnFragmentActivityLRRegister) as Button
+        //var fm: FragmentTransaction =supportFragmentManager.beginTransaction()
+        //fm.replace(R.id.flFragmentActivityLRFragmentsView,FragmentLogin())
+
+        login.setOnClickListener{
+            Log.d("main","prijavi se")
+            var fm: FragmentTransaction =supportFragmentManager.beginTransaction()
+
+            fm.replace(R.id.flFragmentActivityLRFragmentsView,FragmentLogin())
+            fm.commit()
+        }
+
+        register.setOnClickListener{
+            Log.d("main","prijavi se")
+
+            var fm: FragmentTransaction =supportFragmentManager.beginTransaction()
+
+            fm.replace(R.id.flFragmentActivityLRFragmentsView, FragmentRegister())
+            fm.commit()
+        }
     }
+
 }
--- Client/BrzoDoLokacije/app/src/main/java/com/example/brzodolokacije/MainActivity.kt
+++ Client/BrzoDoLokacije/app/src/main/java/com/example/brzodolokacije/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.brzodolokacije
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import androidx.fragment.app.FragmentTransaction
+import com.example.brzodolokacije.Activities.ActivityLoginRegister
+import com.example.brzodolokacije.Fragments.FragmentLogin
+import com.example.brzodolokacije.Fragments.FragmentRegister
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intent= Intent(this, ActivityLoginRegister::class.java)
+        startActivity(intent)
     }
 }
