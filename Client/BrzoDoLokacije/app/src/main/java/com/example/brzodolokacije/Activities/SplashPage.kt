a8c1194dd3ec6683548d8e8fe0bb24c4b8974454
TAMARA JERINIC <tamara.jerinic@gmail.com>
2022-11-02 23:36:21 +0100

    Dodata splash page.

--- Client/BrzoDoLokacije/app/src/main/java/com/example/brzodolokacije/Activities/SplashPage.kt
+++ Client/BrzoDoLokacije/app/src/main/java/com/example/brzodolokacije/Activities/SplashPage.kt
@@ -1,11 +1,23 @@
 package com.example.brzodolokacije.Activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.example.brzodolokacije.MainActivity
+import com.example.brzodolokacije.R
 
 class SplashPage : AppCompatActivity() {
+    private val time:Long = 4000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_page)
+
+            Handler().postDelayed({
+                startActivity(Intent(this, MainActivity::class.java))
+
+                // close this activity
+                finish() }, time)
     }
 }
