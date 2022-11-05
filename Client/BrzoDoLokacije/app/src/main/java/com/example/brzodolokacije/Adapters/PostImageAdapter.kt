fddf235aaf67921c654757f22a63e1500f0b7bbd
Jelena Petrovic <jelenapetrovic.7119@gmail.com>
2022-11-05 23:20:41 +0100

    zapocet rad na prikazu jedne objave, dodata lista slika #16

--- Client/BrzoDoLokacije/app/src/main/java/com/example/brzodolokacije/Adapters/PostImageAdapter.kt
+++ Client/BrzoDoLokacije/app/src/main/java/com/example/brzodolokacije/Adapters/PostImageAdapter.kt
@@ -1,40 +1,33 @@
 package com.example.brzodolokacije.Adapters
 
+import android.graphics.BitmapFactory
+import android.media.Image
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.brzodolokacije.Models.ListItemModel
 import com.example.brzodolokacije.Models.Post
-import com.example.brzodolokacije.databinding.ListItemBinding
+import com.example.brzodolokacije.databinding.PostImageBinding
 import com.example.brzodolokacije.databinding.PostPreviewBinding
-import java.io.Console
 
-class ShowPostsAdapter (val items : MutableList<Post>)
-    : RecyclerView.Adapter<ShowPostsAdapter.ViewHolder>(){
+class PostImageAdapter(val items : MutableList<java.io.File>)
+    : RecyclerView.Adapter<PostImageAdapter.ViewHolder>(){
     //constructer has one argument - list of objects that need to be displayed
     //it is bound to xml of single item
-    private lateinit var binding: PostPreviewBinding
+    private lateinit var binding: PostImageBinding
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        binding=PostPreviewBinding.inflate(inflater,parent,false)
+        binding= PostImageBinding.inflate(inflater,parent,false)
         return ViewHolder(binding)
     }
     override fun onBindViewHolder(holder: ViewHolder, position: Int){
         //sets components of particular item
         holder.bind(items[position])
-/*        holder.itemView.setOnClickListener {
-            fun listener(position: Int){
-                binding.tvTitle.text="klik"
-            }
-        }*/
     }
     override fun getItemCount() = items.size
-    inner class ViewHolder(itemView : PostPreviewBinding) : RecyclerView.ViewHolder(itemView.root){
-        fun bind(item : Post){
+    inner class ViewHolder(itemView : PostImageBinding) : RecyclerView.ViewHolder(itemView.root){
+        fun bind(item : java.io.File){
             binding.apply {
-                tvTitle.text=item.location.name
-                tvLocationParent.text=item.location.country
-                tvLocationType.text=item.location.type.toString()
+                locationImage.setImageBitmap(BitmapFactory.decodeStream(item.inputStream()))
             }
         }
     }
