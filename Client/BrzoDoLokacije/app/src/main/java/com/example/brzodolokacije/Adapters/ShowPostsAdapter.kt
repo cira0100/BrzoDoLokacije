80c38e7aa9ce1c184ad40ab3ffd17e4ecdd46c04
Jelena Petrovic <jelenapetrovic.7119@gmail.com>
2022-11-06 00:47:50 +0100

    Dodato ucitavanje svih objava zahtevom, bez ucitavanja slika #17

--- Client/BrzoDoLokacije/app/src/main/java/com/example/brzodolokacije/Adapters/ShowPostsAdapter.kt
+++ Client/BrzoDoLokacije/app/src/main/java/com/example/brzodolokacije/Adapters/ShowPostsAdapter.kt
@@ -1,15 +1,17 @@
 package com.example.brzodolokacije.Adapters
 
+import android.graphics.BitmapFactory
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.brzodolokacije.Models.ListItemModel
 import com.example.brzodolokacije.Models.Post
+import com.example.brzodolokacije.Models.PostPreview
 import com.example.brzodolokacije.databinding.ListItemBinding
 import com.example.brzodolokacije.databinding.PostPreviewBinding
 import java.io.Console
 
-class ShowPostsAdapter (val items : MutableList<Post>)
+class ShowPostsAdapter (val items : MutableList<PostPreview>)
     : RecyclerView.Adapter<ShowPostsAdapter.ViewHolder>(){
     //constructer has one argument - list of objects that need to be displayed
     //it is bound to xml of single item
@@ -30,7 +32,7 @@ class ShowPostsAdapter (val items : MutableList<Post>)
     }
     override fun getItemCount() = items.size
     inner class ViewHolder(itemView : PostPreviewBinding) : RecyclerView.ViewHolder(itemView.root){
-        fun bind(item : Post){
+        fun bind(item : PostPreview){
             binding.apply {
                 tvTitle.text=item.location.name
                 tvLocationParent.text=item.location.country
