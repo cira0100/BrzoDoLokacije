89d526c9e9d28bd2802d9033318e9eac3d507e34
Ognjen Cirkovic <ciraboxkg@gmail.com>
2022-11-07 22:31:17 +0100

    Napravljen kontroler za preuzimanje aplikacije.

---
+++ Backend/Api/Api/Controllers/appController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class appController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public appController(IConfiguration configuration) { 
+            _configuration = configuration;
+        }
+
+        [HttpGet("download")]
+        public async Task<ActionResult> getApp()
+        {
+            string appPath = _configuration.GetSection("AppSettings:AppName").Value;
+            if (appPath == null || !System.IO.File.Exists(appPath))
+                return BadRequest("Aplikacija ne postoji");
+            return File(System.IO.File.ReadAllBytes(appPath), "application/octet-stream", Path.GetFileName(appPath));
+        }
+
+    }
+}
