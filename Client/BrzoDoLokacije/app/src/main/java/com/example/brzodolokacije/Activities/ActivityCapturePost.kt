ac568dc1cbe62391ab1bdc739c3ecca5eb525800
TAMARA JERINIC <tamara.jerinic@gmail.com>
2022-11-08 02:15:17 +0100

    Dodata aktivnost za fotografisanje, omogućeno čuvanje fotografija.

---
+++ Client/BrzoDoLokacije/app/src/main/java/com/example/brzodolokacije/Activities/ActivityCapturePost.kt
@@ -0,0 +1,11 @@
+package com.example.brzodolokacije.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityCapturePost : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_capture_post)
+    }
+}
