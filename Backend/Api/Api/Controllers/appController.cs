71bbfd0c467118b6fa2162ea85290f244f88353e
Ognjen Cirkovic <ciraboxkg@gmail.com>
2022-11-09 23:48:30 +0100

    Napravljena html stranica sa formom za preuzimanje aplikacije. Dodata provera lozinke prilokom preuzimanja aplikacije.

--- Backend/Api/Api/Controllers/appController.cs
+++ Backend/Api/Api/Controllers/appController.cs
@@ -14,14 +14,28 @@ namespace Api.Controllers
             _configuration = configuration;
         }
 
-        [HttpGet("download")]
-        public async Task<ActionResult> getApp()
+        [HttpPost("download")]
+        public async Task<ActionResult> getApp([FromForm] string password)
         {
             string appPath = _configuration.GetSection("AppSettings:AppName").Value;
+            string truePassword = _configuration.GetSection("AppSettings:AppPassword").Value;
             if (appPath == null || !System.IO.File.Exists(appPath))
                 return BadRequest("Aplikacija ne postoji");
+            if (password == null || password!=truePassword)
+                return BadRequest("Pogresna sifra");
             return File(System.IO.File.ReadAllBytes(appPath), "application/octet-stream", Path.GetFileName(appPath));
         }
+        [HttpGet("download")]
+        public async Task<ActionResult<string>> getForm()
+        {
 
+                var html = await System.IO.File.ReadAllTextAsync(@"./Assets/appDownload.html");
+                html = html.Replace("{{name}}", "test");
+
+                return base.Content(html, "text/html");
+            
+        }
     }
+
 }
+
