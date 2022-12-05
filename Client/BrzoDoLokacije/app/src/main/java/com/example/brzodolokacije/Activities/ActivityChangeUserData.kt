1bf506055800219feb3ab33be2e2caf8ab639856
TAMARA JERINIC <tamara.jerinic@gmail.com>
2022-12-05 22:45:58 +0100

    Dodata aktivnost za izmenu podataka korisnika.

---
+++ Client/BrzoDoLokacije/app/src/main/java/com/example/brzodolokacije/Activities/ActivityChangeUserData.kt
@@ -0,0 +1,11 @@
+package com.example.brzodolokacije.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityChangeUserData : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change_user_data)
+    }
+}
