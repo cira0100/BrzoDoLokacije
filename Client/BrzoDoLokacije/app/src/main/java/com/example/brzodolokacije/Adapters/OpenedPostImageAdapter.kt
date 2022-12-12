6c42703495afaa9c9804d6d94b74fddde8d2a4b2
Jelena Petrovic <jelenapetrovic.7119@gmail.com>
2022-12-12 03:51:45 +0100

    Resen bag pogresne rotacije slika #83

--- Client/BrzoDoLokacije/app/src/main/java/com/example/brzodolokacije/Adapters/OpenedPostImageAdapter.kt
+++ Client/BrzoDoLokacije/app/src/main/java/com/example/brzodolokacije/Adapters/OpenedPostImageAdapter.kt
@@ -5,12 +5,15 @@ import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView
 import com.example.brzodolokacije.Models.PostImage
 import com.example.brzodolokacije.Services.RetrofitHelper
 import com.example.brzodolokacije.databinding.OpenedPostImageBinding
 import com.github.piasy.biv.BigImageViewer
+import com.github.piasy.biv.loader.ImageLoader
 import com.github.piasy.biv.loader.glide.GlideImageLoader
 import com.github.piasy.biv.view.BigImageView
+import java.io.File
 
 class OpenedPostImageAdapter(val items:List<PostImage>?,val activity:Activity): RecyclerView.Adapter<OpenedPostImageAdapter.ViewHolder>() {
     lateinit var binding:OpenedPostImageBinding
@@ -18,7 +21,32 @@ class OpenedPostImageAdapter(val items:List<PostImage>?,val activity:Activity): 
     inner class ViewHolder(itemView: OpenedPostImageBinding) : RecyclerView.ViewHolder(itemView.root) {
         fun bind(item:PostImage){
             binding.apply {
-                ivOpenedImage.setInitScaleType(BigImageView.INIT_SCALE_TYPE_FIT_START)
+                ivOpenedImage.setImageLoaderCallback(object : ImageLoader.Callback{
+                    override fun onCacheHit(imageType: Int, image: File?) {
+                        ivOpenedImage.ssiv.orientation = SubsamplingScaleImageView.ORIENTATION_USE_EXIF
+                    }
+
+                    override fun onCacheMiss(imageType: Int, image: File?) {
+                    }
+
+                    override fun onStart() {
+                    }
+
+                    override fun onProgress(progress: Int) {
+                    }
+
+                    override fun onFinish() {
+                        ivOpenedImage.ssiv.orientation = SubsamplingScaleImageView.ORIENTATION_USE_EXIF
+                    }
+
+                    override fun onSuccess(image: File?) {
+                    }
+
+                    override fun onFail(error: Exception?) {
+                    }
+
+                })
+                ivOpenedImage.setInitScaleType(BigImageView.INIT_SCALE_TYPE_CENTER_CROP)
                 ivOpenedImage.showImage(Uri.parse(RetrofitHelper.baseUrl + "/api/post/image/compress/" + item._id))
             }
         }
